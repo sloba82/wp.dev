9dd2294572b0087504d3d61c6be23c84bd97b6a7
sloba82 <dizajnistampa@gmail.com>
2020-12-03 21:44:45 +0100

    home page services refactoring

--- wp-content/themes/eyelesh/index.php
+++ wp-content/themes/eyelesh/index.php
@@ -65,53 +65,6 @@ $loop = new WP_Query( array(
     </div>
 </section>
 
-
-
-
-<!-- Services-->
-<section class="page-section" id="services">
-    <div class="container">
-        <h2 class="text-center mt-0">Usluge</h2>
-        <hr class="divider my-4" />
-
-        <div class="card-deck">
-            <div class="card">
-                <img class="card-img-top"
-                    src="<?php bloginfo('template_directory'); ?>/assets/img/mt-1429-pricing-icon-1.png"
-                    alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                        additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                </div>
-            </div>
-            <div class="card">
-                <img class="card-img-top"
-                    src="<?php bloginfo('template_directory'); ?>/assets/img/mt-1429-pricing-icon-1.png"
-                    alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.
-                    </p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                </div>
-            </div>
-            <div class="card">
-                <img class="card-img-top"
-                    src="<?php bloginfo('template_directory'); ?>/assets/img/mt-1429-pricing-icon-1.png"
-                    alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
-                        additional content. This card has even longer content than the first to show that equal height
-                        action.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
 <!-- Portfolio-->
 <div id="portfolio">
     <div class="container-fluid p-0">
